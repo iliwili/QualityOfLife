5a1962ef5df3ef5bc930267184f935186016876c
iliwili <ilias0258@gmail.com>
2021-03-06 13:46:25 +0100

    Fixed the last feature, and refactored the code.

--- QualityOfLife/QualityOfLife.cs
+++ QualityOfLife/QualityOfLife.cs
@@ -2,6 +2,8 @@
 using BepInEx.Logging;
 using BepInEx.Configuration;
 using HarmonyLib;
+using System.IO;
+using System.Reflection;
 
 namespace QualityOfLife
 {
@@ -9,7 +11,8 @@ namespace QualityOfLife
     [HarmonyPatch]
     public class QualityOfLife : BaseUnityPlugin
     {
-        private static ManualLogSource Log;
+        public static ManualLogSource Log;
+        private Harmony Harmony;
         // [SMELTER]
         public static ConfigEntry<bool> EnableSmelterModification;
 
@@ -22,7 +25,7 @@ namespace QualityOfLife
         // [SMELTER.BLASTFURNACE]
         public static ConfigEntry<int> BlastFurnaceOreMultiplier;
         public static ConfigEntry<int> BlastFurnaceFuelMultiplier;
-        public static ConfigEntry<int> BlastFurnacePerProduct;
+        public static ConfigEntry<int> BlastFurnaceFuelPerProduct;
         public static ConfigEntry<int> BlastFurnaceSecPerProduct;
 
         // [SMELTER.CHARCOALKILN]
@@ -34,7 +37,9 @@ namespace QualityOfLife
             Log = this.Logger;
             BepInEx.Logging.Logger.Sources.Add(Log);
 
-            new Harmony("iliwili.quality_of_life").PatchAll();
+            this.Harmony = new Harmony("iliwili.quality_of_life");
+
+            Harmony.CreateAndPatchAll(typeof(SmelterMod));
             Log.LogInfo("QualityOfLife loaded.");
         }
 
@@ -44,74 +49,16 @@ namespace QualityOfLife
 
             SmelterOreMultiplier = this.Config.Bind<int>("SMELTER.SMELTER", "SmelterOreMultiplier", 2, "Smelter ore multiplier");
             SmelterFuelMultiplier = this.Config.Bind<int>("SMELTER.SMELTER", "SmelterFuelMultiplier", 2, "Smelter fuel multiplier");
-            // TODO: This doesn't work in the config file
-            SmelterFuelPerProduct = this.Config.Bind<int>("SMELTER.SMELTER", "SmelterFuelPerProduct", 2, "Smelter fuel per burnt ore (lowest is 1)");
-            SmelterSecPerProduct = this.Config.Bind<int>("SMELTER.SMELTER", "SmelterSecPerProduct", 5, "Smelter processing time per ore (lowest is 1)");
+            SmelterFuelPerProduct = this.Config.Bind<int>("SMELTER.SMELTER", "SmelterFuelPerProduct", 1, "Smelter fuel per burnt ore (lowest is 1 but it will still take 2 fuel per product, don't actualy know why it does that.)");
+            SmelterSecPerProduct = this.Config.Bind<int>("SMELTER.SMELTER", "SmelterSecPerProduct", 1, "Smelter processing time per ore (lowest is 1)");
 
             BlastFurnaceOreMultiplier = this.Config.Bind<int>("SMELTER.BLASTFURNACE", "BlastFurnaceOreMultiplier", 2, "Blast Furnace ore multiplier");
             BlastFurnaceFuelMultiplier = this.Config.Bind<int>("SMELTER.BLASTFURNACE", "BlastFurnaceFuelMultiplier", 2, "Blast Furnace fuel multiplier");
-            BlastFurnacePerProduct = this.Config.Bind<int>("SMELTER.BLASTFURNACE", "BlastFurnacePerProduct", 2, "Blast furnace fuel per burnt ore (lowest is 1)");
-            BlastFurnaceSecPerProduct = this.Config.Bind<int>("SMELTER.BLASTFURNACE", "BlastFurnaceSecPerProduct", 5, "Blast furnace processing time per ore (lowest is 1)");
+            BlastFurnaceFuelPerProduct = this.Config.Bind<int>("SMELTER.BLASTFURNACE", "BlastFurnacePerProduct", 1, "Blast furnace fuel per burnt ore (lowest is 1 but it will still take 2 fuel per product, don't actualy know why it does that.)");
+            BlastFurnaceSecPerProduct = this.Config.Bind<int>("SMELTER.BLASTFURNACE", "BlastFurnaceSecPerProduct", 1, "Blast furnace processing time per ore (lowest is 1)");
 
             CharcoalKilnMultiplier = this.Config.Bind<int>("SMELTER.CHARCOALKILN", "CharcoalKilnMultiplier", 2, "Charcoal Kiln multiplier");
-            CharcoalKilnSecPerProduct = this.Config.Bind<int>("SMELTER.CHARCOALKILN", "CharcoalKilnSecPerProduct", 2, "Charcoal Kiln processing time per wood (lowest is 1)");
-        }
-
-        // TODO: Fix a seperate class for the smelters function
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(Smelter), "Awake")]
-        public static void StartSmelter(ref Smelter __instance)
-        {
-            if (EnableSmelterModification.Value)
-            {
-                switch (__instance.m_name)
-                {
-                    case "$piece_smelter":
-                        __instance.m_maxOre *= SmelterOreMultiplier.Value;
-                        __instance.m_maxFuel *= SmelterFuelMultiplier.Value;
-
-                        if (SmelterFuelPerProduct.Value > 1 && SmelterSecPerProduct.Value > 1)
-                        {
-                            __instance.m_fuelPerProduct = SmelterFuelPerProduct.Value;
-                            __instance.m_secPerProduct = SmelterSecPerProduct.Value;
-                        } else
-                        {
-                            Log.LogInfo("Smelter fuel per product or smelter sec per product is too low, lowest is 1.");
-                        }
-                        break;
-                    case "$piece_blastfurnace":
-                        __instance.m_maxOre *= BlastFurnaceOreMultiplier.Value;
-                        __instance.m_maxFuel *= BlastFurnaceFuelMultiplier.Value;
-
-                        if (BlastFurnacePerProduct.Value > 1 && BlastFurnaceSecPerProduct.Value > 1)
-                        {
-                            __instance.m_fuelPerProduct = BlastFurnacePerProduct.Value;
-                            __instance.m_secPerProduct = BlastFurnaceSecPerProduct.Value;
-                        }
-                        else
-                        {
-                            Log.LogInfo("Blast Furnace fuel per product or smelter sec per product is too low, lowest is 1.");
-                        }
-                        break;
-                    case "$piece_charcoalkiln":
-                        __instance.m_maxOre *= CharcoalKilnMultiplier.Value;
-
-                        if (CharcoalKilnSecPerProduct.Value > 1)
-                        {
-                            __instance.m_secPerProduct = CharcoalKilnSecPerProduct.Value;
-                        }
-                        else
-                        {
-                            Log.LogInfo("Charcoal Kiln sec per product is too low, lowest is 1.");
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            } else
-            {
-                Log.LogInfo("Smelter mod is disabled.");
-            }
+            CharcoalKilnSecPerProduct = this.Config.Bind<int>("SMELTER.CHARCOALKILN", "CharcoalKilnSecPerProduct", 1, "Charcoal Kiln processing time per wood (lowest is 1 but it will still take more than 1 second, don't actualy know why it does that.)");
         }
     }
 }
---
+++ QualityOfLife/SmelterMod.cs
@@ -0,0 +1,66 @@
+﻿using HarmonyLib;
+
+namespace QualityOfLife
+{
+    class SmelterMod
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Smelter), "Awake")]
+        public static void StartSmelter(ref Smelter __instance)
+        {
+            if (QualityOfLife.EnableSmelterModification.Value)
+            {
+                QualityOfLife.Log.LogInfo(__instance.m_name);
+                switch (__instance.m_name)
+                {
+                    case "$piece_smelter":
+                        __instance.m_maxOre *= QualityOfLife.SmelterOreMultiplier.Value;
+                        __instance.m_maxFuel *= QualityOfLife.SmelterFuelMultiplier.Value;
+
+                        if (QualityOfLife.SmelterFuelPerProduct.Value >= 1 && QualityOfLife.SmelterSecPerProduct.Value >= 1)
+                        {
+                            __instance.m_fuelPerProduct = QualityOfLife.SmelterFuelPerProduct.Value;
+                            __instance.m_secPerProduct = QualityOfLife.SmelterSecPerProduct.Value;
+                        }
+                        else
+                        {
+                            QualityOfLife.Log.LogInfo("Smelter fuel per product or smelter sec per product is too low, lowest is 1.");
+                        }
+                        break;
+                    case "$piece_blastfurnace":
+                        __instance.m_maxOre *= QualityOfLife.BlastFurnaceOreMultiplier.Value;
+                        __instance.m_maxFuel *= QualityOfLife.BlastFurnaceFuelMultiplier.Value;
+
+                        if (QualityOfLife.BlastFurnaceFuelPerProduct.Value >= 1 && QualityOfLife.BlastFurnaceSecPerProduct.Value >= 1)
+                        {
+                            __instance.m_fuelPerProduct = QualityOfLife.BlastFurnaceFuelPerProduct.Value;
+                            __instance.m_secPerProduct = QualityOfLife.BlastFurnaceSecPerProduct.Value;
+                        }
+                        else
+                        {
+                            QualityOfLife.Log.LogInfo("Blast Furnace fuel per product or smelter sec per product is too low, lowest is 1.");
+                        }
+                        break;
+                    case "$piece_charcoalkiln":
+                        __instance.m_maxOre *= QualityOfLife.CharcoalKilnMultiplier.Value;
+
+                        if (QualityOfLife.CharcoalKilnSecPerProduct.Value >= 1)
+                        {
+                            __instance.m_secPerProduct = QualityOfLife.CharcoalKilnSecPerProduct.Value;
+                        }
+                        else
+                        {
+                            QualityOfLife.Log.LogInfo("Charcoal Kiln sec per product is too low, lowest is 1.");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                QualityOfLife.Log.LogInfo("Smelter mod is disabled.");
+            }
+        }
+    }
+}
